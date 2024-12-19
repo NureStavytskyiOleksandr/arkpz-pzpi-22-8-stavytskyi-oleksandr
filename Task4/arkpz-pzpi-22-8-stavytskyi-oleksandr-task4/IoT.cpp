dda694b8c98d5e5e4902cd1d89d183b381bc18fa
Oleksandr Stavytskyi <oleksandr.stavytskyi@nure.ua>
2024-12-19 16:20:57 +0200

    Update IoT.cpp

--- Task4/arkpz-pzpi-22-8-stavytskyi-oleksandr-task4/IoT.cpp
+++ Task4/arkpz-pzpi-22-8-stavytskyi-oleksandr-task4/IoT.cpp
@@ -8,14 +8,14 @@ int echoPin = 22;
 int ledPinGreen = 18;
 int ledPinRed = 19;
 
-const char* ssid = "Wokwi-GUEST";  
-const char* password = ""; 
+char* ssid = "Wokwi-GUEST";  
+char* password = ""; 
 
-const char* mqtt_server = "broker.emqx.io"; 
-const int mqtt_port = 1883;
-const char* mqtt_user = ""; 
-const char* mqtt_password = ""; 
-const char* mqtt_topic = "parking/status";  
+char* mqtt_server = "broker.emqx.io"; 
+int mqtt_port = 1883;
+char* mqtt_user = ""; 
+char* mqtt_password = ""; 
+char* mqtt_topic = "parking/status";  
 
 
 WiFiClient espClient;
@@ -76,7 +76,8 @@ void loop() {
 
 
   unsigned long duration = pulseIn(echoPin, HIGH);
-  // БІЗНЕС ЛОГІКА (розрахунок дистанції)
+
+
   long distance = duration * 0.0344 / 2;
 
 
